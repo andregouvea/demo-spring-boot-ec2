f293070870ac19f951e7b710ec5a8ac7d30d2c22
andregouvea <andre.gouvea@gmail.com>
2017-03-27 13:48:00 -0300

    without instance metadata

--- src/main/java/br/com/pirre/DemoController.java
+++ src/main/java/br/com/pirre/DemoController.java
@@ -18,19 +18,25 @@ public class DemoController {
 	//Code for demonstration only, ***Kids, don't try this at home*** :)
 	@RequestMapping("/status")
 	public String status() throws Exception{
-		StringBuilder result = new StringBuilder();
-		URL url = new URL("http://169.254.169.254/latest/meta-data/instance-id");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
+		try{
+			StringBuilder result = new StringBuilder();
+			URL url = new URL("http://169.254.169.254/latest/meta-data/instance-id");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(1000);
+			conn.setRequestMethod("GET");
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			rd.close();
+
+			return new Date().toString() + " ID do servidor: " + result.toString();
+			
+		}catch (Exception ex){
 		}
-		rd.close();
-
-		return new Date().toString() + " ID do servidor: " + result.toString();
-
+		
+		return "OK";
 	}
 
 
